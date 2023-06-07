7031a4a6bfdf2cfc33cb4c0aa7bb341c0ae307f7
jbarz <jbarz0415@gmail.com>
2023-06-07 11:09:27 -0400

    Adding display code

--- mini-activity/assets/js/script.js
+++ mini-activity/assets/js/script.js
@@ -1,9 +1,13 @@
-var form = document.getElementById("form");
+var form = document.getElementById("htmlForm");
 
 var firstNameEl = document.querySelector("#first-name");
 var lastNameEl = document.querySelector("#last-name");
 var emailEl = document.querySelector("#email");
 var passwordEl = document.querySelector("#password");
+var ulEl = document.querySelector("#creds");
+
+var firstNameDisplay = document.createElement("li");
+var lastNameDisplay = document.createElement("li");
 
 function handleFormSubmit (event) {
     event.preventDefault();
@@ -19,6 +23,12 @@ function handleFormSubmit (event) {
 
     var password = passwordEl.value;
     console.log("Password: " + password)
+
+    firstNameDisplay.textContent = firstName;
+    ulEl.appendChild(firstNameDisplay);
+
+    lastNameDisplay.textContent = lastName;
+    ulEl.appendChild(lastNameDisplay);
 }
 
 form.addEventListener("submit", handleFormSubmit);
